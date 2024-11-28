21c177561c7763259196a92d622b24fc2102a0c1
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-11-28 11:17:54 -0800

    a6 MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
@@ -9,6 +11,9 @@ import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
