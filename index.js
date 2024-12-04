75e608873050b295cc62873dfde360716f6237ba
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-12-03 18:19:35 -0800

    updated connection string for mongo db

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
-// || "mongodb://127.0.0.1:27017/kanbas";
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
